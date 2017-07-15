e02ffdfa0d3ec354bca4a3dc16d6da2e6a2686dc
SHASHANKA1996 <shashankarao1996@gmail.com>
2017-07-15 15:47:04 +0530

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,6 @@
+/*object literal notation provide by different objects are separated by coma*/
+var obj = {name:"shashank", age:"21", dob:'30/05/1996',place:"xyz"};
+document.getElementById("NAME").innerHTML = obj.name;
+document.getElementById("AGE").innerHTML = obj.age;
+document.getElementById("DOB").innerHTML = obj.dob;
+document.getElementById("POB").innerHTML = obj.place;
